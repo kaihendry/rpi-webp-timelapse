17f5893eeb8945921ee19041a30552d29a675bef
Kai Hendry <hendry@iki.fi>
2014-07-03 19:08:11 +0100

    Tweaks

--- snapshot.sh
+++ snapshot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+umask 002
 tmp=$(mktemp /tmp/XXX.jpg)
 webp=$(basename $tmp .jpg).webp
-/opt/vc/bin/raspistill -n -hf -rot 270 $@ -o $tmp
+/opt/vc/bin/raspistill -n $@ -o $tmp
 dir=/home/hendry/camera/$(date +%Y-%m-%d)/$(cat /proc/sys/kernel/random/boot_id)
 mkdir -p $dir 2>/dev/null || true
 output=$dir/$(date +%s).webp
 cwebp -quiet $tmp -o $output
+ln -sf $output /srv/http/index.webp
 rm -f $tmp
